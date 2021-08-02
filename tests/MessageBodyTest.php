a99be2d5ce8a7635e5be13906edddd248f832e87
hexonthebeach <nielsverbaken@outlook.com>
2021-08-02 14:31:56 +0200

    Added support for setting bodytype and dcs

--- tests/MessageBodyTest.php
+++ tests/MessageBodyTest.php
@@ -8,36 +8,93 @@ use PHPUnit\Framework\TestCase;
 class MessageBodyTest extends TestCase
 {
 
+    public function testJsonSerialization()
+    {
+        $messageBody = new MessageBody('content');
+
+        // Should be Json Serializable with or without multi-byte-string extension
+        $json = json_encode($messageBody);
+        $this->assertJson(
+            $json
+        );
+    }
+
     /**
      * Result of setting up a new MessageBody
      */
-    public function testReadingProperties()
+    public function testSettingContent()
     {
         $content = 'test message body content';
 
         $messageBody = new MessageBody($content);
 
+        $json = json_encode($messageBody);
+
         // Content should be set correctly
         $this->assertEquals(
             $content,
-            $messageBody->content
-        );
-
-        // Type should be set as expected
-        $this->assertEquals(
-            \CMText\MessageBodyTypes::AUTO,
-            $messageBody->type
-        );
-
-        // unknown properties should return false
-        $this->assertFalse(
-            $messageBody->unknownProperty
-        );
-
-        // Should be Json Serializable with or without multi-byte-string extension
-        $this->assertJson(
-            json_encode($messageBody)
+            (json_decode($json))->content
         );
     }
 
+
+    /**
+     * Result of setting up a new MessageBodyType
+     */
+    public function testSettingTypeText()
+    {
+        $messageBody = new MessageBody(__METHOD__, \CMText\MessageBodyTypes::TEXT);
+
+        $json = json_encode($messageBody);
+
+        // Type should be set correctly
+        $this->assertEquals(
+            \CMText\MessageBodyTypes::TEXT,
+            (json_decode($json))->type
+        );
+    }
+
+
+    /**
+     * Result of setting up a new MessageBodyType Incorrectly
+     */
+    public function testSettingTypeIncorrectly()
+    {
+        $messageBody = new MessageBody('content', 'invalid-type');
+
+        $json = json_encode($messageBody);
+
+        // Type should be set to AUTO
+        $this->assertEquals(
+            \CMText\MessageBodyTypes::AUTO,
+            (json_decode($json))->type
+        );
+
+
+        $messageBody->type = 'still-invalid';
+
+        $json = json_encode($messageBody);
+
+        // Type should be set to AUTO
+        $this->assertEquals(
+            \CMText\MessageBodyTypes::AUTO,
+            (json_decode($json))->type
+        );
+    }
+
+
+    public function testWithTypeChaining()
+    {
+        $messageBody = (new MessageBody('content'))->WithType(\CMText\MessageBodyTypes::BINARY);
+
+        $this->assertInstanceOf(
+            MessageBody::class,
+            $messageBody
+        );
+
+        $this->assertEquals(
+            \CMText\MessageBodyTypes::BINARY,
+            $messageBody->jsonSerialize()->type
+        );
+    }
 }
