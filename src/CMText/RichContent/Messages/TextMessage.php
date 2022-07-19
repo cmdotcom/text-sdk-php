a9162a46a4327900418a89e96a11c7e2d9ecf19b
Joep Beusenberg <joep.beusenberg@cm.com>
2022-07-19 12:35:37 +0400

    Add Instagram message tags

--- src/CMText/RichContent/Messages/TextMessage.php
+++ src/CMText/RichContent/Messages/TextMessage.php
@@ -16,20 +16,29 @@ class TextMessage implements IRichMessage
 
 
     /**
+     * @var string Instagram message tag (optional)
+     */
+    protected $tag;
+
+
+    /**
      * TextMessage constructor.
      * @param string $Text
+     * @param string $Tag
      */
-    public function __construct(string $Text)
+    public function __construct(string $Text, string $Tag = '')
     {
         $this->text = $Text;
+        $this->tag = $Tag;
     }
 
 
 	#[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
-        return (object)[
+        return (object)array_filter([
             'text' => $this->text
-        ];
+            'tag' => $this->tag
+        ]);
     }
 }
