b0a0d1d455254ec4370ec1e53fcb70097ccbc7be
Niels Verbaken <nv@cm.nl>
2024-06-17 15:32:15 +0200

    Updated global gateway-endpoints

--- src/CMText/Gateways.php
+++ src/CMText/Gateways.php
@@ -14,13 +14,14 @@ class Gateways
 {
 
     /**
-     * Gateway that fits most use cases
+     * Recommended Global endpoint via Cloudflare.
+     *
+     * If you use the Global endpoint via Cloudflare, you agree that Cloudflare, Inc. located in the USA is engaged as a Sub-processor under the Agreement, for an overview of our Sub-processors please see: https://www.cm.com/cdn/web/en/file/subprocessors.pdf .
      */
     const GLOBAL = 'https://gw.messaging.cm.com/v1.0/message';
 
     /**
-     * South Africa local gateway
+     * Alternative Global endpoint
      */
-    const ZA = 'https://gw.cmtelecom.co.za/v1.0/message';
-
+    const GLOBAL_ALTERNATIVE = 'https://gw.cmtelecom.com/v1.0/message';
 }
